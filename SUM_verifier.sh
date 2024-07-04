402e9f0c4d9a64dc54da90ee671f138ee7f5e7d6
Lmuhammed <140099085+Lmuhammed@users.noreply.github.com>
2024-07-04 18:44:28 +0100

    support for sha1 and sha256 added

--- SUM_verifier.sh
+++ SUM_verifier.sh
@@ -24,16 +24,21 @@ fi
 #end check 
 
 #No arrguments :
-if [ "$#" -lt 1 ]; then
+if [ "$#" -lt 3 ]; then
 echo "Sum veriefer"
-echo "Usage: $0 {program} [md5Hash]"
+echo "Usage: $0 [checksumAlgo] {program} [md5Hash]"
+echo "checksumAlgo :"
+echo "md5          :"
+echo "s1           :    sha1"
+echo "s2           :    sha256"
 echo -e "${BGreen}Exemples : ${Reset}"
 echo "$0 $HOME/Dowloads/app.sh      6d18504f70aa38ae1e6e17b2b791d874"
 echo "$0 ./app.sh                   6d18504f70aa38be1e6e17b2b791d874"
 exit 2
 else
-program=$1
-program_checksum=$2
+checksumAlgo=$1
+program=$2
+program_checksum=$3
 fi
 
 # *) check if programme exist
@@ -42,7 +47,25 @@ if ! test -f "$program"; then
     exit 2
 fi
 # Calculate the actual checksum
-actual_checksum=$(md5sum "$program" | awk '{print $1}')
+#actual_checksum=$(md5sum "$program" | awk '{print $1}')
+
+# Case statement to check the checksumAlgo
+case $checksumAlgo in
+    "md5")
+        actual_checksum=$(md5sum "$program" | awk '{print $1}')
+        ;;
+    "s1")
+        actual_checksum=$(sha1sum "$program" | awk '{print $1}')
+        ;;
+    "s2")
+        actual_checksum=$(sha256sum "$program" | awk '{print $1}')
+        ;;
+    *)
+        echo -e "${BRed}Invalid Checksum ${Reset}"
+        exit 2
+        ;;
+esac
+
 #clear the screen for better focus & print result
 clear
 echo "The Programme             : $program"
